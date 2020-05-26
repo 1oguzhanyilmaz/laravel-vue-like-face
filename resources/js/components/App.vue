bed036151ea713118777dcc63ff29379b18c6ada
Oğuzhan Yılmaz <45510538+1oguzhanyilmaz@users.noreply.github.com>
2020-05-26 12:12:54 +0300

    app design with tailwindcss

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,28 @@
 <template>
-    <div>App <router-view></router-view></div>
+    <div class="flex flex-col flex-1 h-screen overflow-y-scroll">
+        <app-nav></app-nav>
+        <div class="flex overflow-y-hidden flex-1">
+            <app-sidebar></app-sidebar>
+
+            <div class="w-2/3 overflow-x-hidden">
+                <router-view></router-view>
+                aBcdef 123
+            </div>
+        </div>
+    </div>
 
 </template>
 
 <script>
+    import Nav from "./Nav";
+    import Sidebar from "./Sidebar";
+
     export default {
-        name: "App"
+        name: "App",
+        components: {
+            "app-nav": Nav,
+            "app-sidebar": Sidebar,
+        }
     }
 </script>
 
---
+++ resources/js/views/NewsFeed.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col items-center py-4">
+        <app-new-post></app-new-post>
+        <app-post></app-post>
+    </div>
+</template>
+
+<script>
+    import NewPost from "../components/NewPost";
+    import Post from "../components/Post";
+
+    export default {
+        name: "NewsFeed",
+        components: {
+            "app-new-post": NewPost,
+            "app-post": Post,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
